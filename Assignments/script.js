a2bfb65b73a12a1fd48318b0b225e132295f1cc8
balaji <malabalaji7899@gmail.com>
2021-10-27 09:42:03 +0530

    first commit inside js-script branch

---
+++ Assignments/script.js
@@ -0,0 +1,11 @@
+//this is js-script branch
+switch(expression) {
+    case x:
+      // code block
+      break;
+    case y:
+      // code block
+      break;
+    default:
+      // code block
+  }
